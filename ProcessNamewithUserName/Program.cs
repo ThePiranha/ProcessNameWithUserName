94a8f35a82d862799f4310d592e7de232d6c4057
ThePiranha <sabah_365@hotmail.com>
2017-03-01 09:04:55 -0500

    Uploading project

---
+++ ProcessNamewithUserName/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Management;
+using System.Diagnostics;
+
+namespace ProcessNamewithUserName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string query = "Select * From Win32_Process";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ManagementObjectCollection processList = searcher.Get();
+
+            Console.WriteLine("Process Name" + "   " + "Process Name" + "           " + "UserName");
+            Console.WriteLine("===================================================================");
+
+            foreach (ManagementObject result in processList)
+            {
+                string[] argList = new string[] { string.Empty, string.Empty };
+                int returnVal = Convert.ToInt32(result.InvokeMethod("GetOwner", argList));
+               
+                if (returnVal == 0)
+                {
+                    Console.WriteLine(result["ProcessId"] + "             " +  result["Name"] + "  -->  " + argList[1] + "\\" + argList[0]);
+                }              
+            }           
+            Console.ReadLine();
+        }
+    }
+}
